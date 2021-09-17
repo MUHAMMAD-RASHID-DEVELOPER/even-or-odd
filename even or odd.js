378e1e2c90e4e9a7cf926cefc242d6418d239b55
M.Rashid <76033808+MUHAMMAD-RASHID-DEVELOPER@users.noreply.github.com>
2021-09-17 12:40:25 +0500

    even or odd

---
+++ even or odd.js
@@ -0,0 +1,7 @@
+var no=prompt("Please input the name to check whether it is even or odd");
+if (no%2==0){
+    alert("number is even")
+}
+else{
+    alert("numver is odd")
+}
---
+++ javascript -html.js
@@ -0,0 +1,11 @@
+// // var h1Tag=document.getElementsByTagName("h1");
+// var input=document.getElementById("input");
+// var h1=document.getElementsByTagName("h1")
+// h1Tag.innerHtml=inputTag.value;
+
+
+function onClickHandler(){
+    var userName=document.getElementById("userName").value;
+    var output="welcome",&(userName);
+    document.getElementById("output").innerHTML=output;
+}
